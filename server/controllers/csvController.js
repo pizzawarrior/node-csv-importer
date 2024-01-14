4b5476814d0c8a15b585fb9d6fd9d549928f4830
pizzawarrior <i.norstad@gmail.com>
2024-01-14 15:49:49 -0800

    csv upload and download controllers, plus employee controller

--- server/controllers/csvController.js
+++ server/controllers/csvController.js
@@ -1,6 +1,7 @@
 import Employee from "../models/employeeModel";
 import { createReadStream } from "fs";
 import { parse } from "fast-csv";
+import { Parser as CsvParser } from "json2csv";
 
 /*
 Pull the data that the middleware sent to local disk storage and parse it, then store it in the db
@@ -46,4 +47,64 @@ const upload = async (req, res) => {
       message: "Failed to upload the file: " + req.file.originalname,
     });
   }
+};
+
+const download = (_req, res) => {
+  Employee.findAll().then((objs) => {
+    let employees = [];
+
+    objs.forEach((obj) => {
+      const {
+        id,
+        name,
+        email,
+        username,
+        dob,
+        company,
+        address,
+        location,
+        salary,
+        about,
+        role,
+      } = obj;
+      employees.push({
+        id,
+        name,
+        email,
+        username,
+        dob,
+        company,
+        address,
+        location,
+        salary,
+        about,
+        role,
+      });
+    });
+
+    const csvFields = [
+      "id",
+      "name",
+      "email",
+      "dob",
+      "company",
+      "address",
+      "location",
+      "salary",
+      "about",
+      "role",
+    ];
+    const csvParser = new CsvParser({ csvFields });
+    const csvData = csvParser.parse(employees);
+
+    res.setHeader("Content-Type", "text/csv");
+    res.setHeader("Content-Disposition", "attachment; filename=employees.csv");
+
+    res.status(200).end(csvData);
+  });
+};
+
+export default {
+  upload,
+  download,
 };
---
+++ server/controllers/employeeController.js
@@ -0,0 +1,28 @@
+import Employee from "../models/employeeModel.js";
+
+const getEmployees = (_req, res) => {
+  Employee.findAll({
+    include: [
+      {
+        model: Employee,
+        as: "children",
+        attributes: ["id", "name", "email", "username", "avatar"],
+        required: true,
+      },
+    ],
+    attributes: {
+      exclude: ["managedBy"],
+    },
+  })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Error while retrieving records from the database",
+      });
+    });
+};
+
+export default getEmployees;
