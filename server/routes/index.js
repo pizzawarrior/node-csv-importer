3302f076dc6f89b72f4bfe4b273074d091291965
pizzawarrior <i.norstad@gmail.com>
2024-01-14 16:08:41 -0800

    build csv and employees routes

---
+++ server/routes/index.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import csvController from "../controllers/csvController";
+import getEmployees from "../controllers/employeeController";
+import uploadFile from "../middleware/upload.js";
+
+const router = Router();
+
+let routes = (app) => {
+  //csv routes
+  router.post("/csv/upload", uploadFile.single("file"), csvController.upload);
+  router.get("/csv/download", csvController.download);
+
+  //employee routes
+  router.get("/employees", getEmployees);
+
+  app.use("/api", router);
+};
+
+export default routes;
